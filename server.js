6fcc50ea1c1dff3491884d6a27d2e4528af18313
copiousamounts <nick.hernstedt@gmail.com>
2020-04-08 12:39:50 -0700

    created resturant models

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
+
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
@@ -13,6 +14,13 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+//handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
@@ -21,8 +29,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
+var routes = require("./controllers/controller.js");
+
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+
+app.use(routes)
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function () {
