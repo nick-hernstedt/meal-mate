1fa17ab93032952edbc92508efee6778614cd023
copiousamounts <nick.hernstedt@gmail.com>
2020-04-11 12:08:10 -0700

    removing some handlebars test stuff

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,23 +9,23 @@ const resturant = require("../models/resturant");
 
 const key = process.env.API_KEY;
 
-// router.post("/api/proxy", function(req, res) {
-//   console.log(`yes`);
-//   axios
-//     .get(
-//       `https://api.yelp.com/v3/businesses/search?term=food&location=${req.body.location}`,
-//       {
-//         headers: {
-//           Authorization: `Bearer ${key}`
-//         },
-//         responseType: `json`
-//       }
-//     )
-//     .then(answer => {
-//       console.log(answer.data);
-//       return res.json(answer.data);
-//     });
-// });
+router.post("/api/proxy", function(req, res) {
+  console.log(`yes`);
+  axios
+    .get(
+      `https://api.yelp.com/v3/businesses/search?term=food&location=${req.body.location}`,
+      {
+        headers: {
+          Authorization: `Bearer ${key}`
+        },
+        responseType: `json`
+      }
+    )
+    .then(answer => {
+      console.log(answer.data);
+      return res.json(answer.data);
+    });
+});
 
 router.get("/main", function(req, res) {
   res.render("index", req.user);
@@ -39,41 +39,18 @@ router.post("/api/match", function(req, res) {
   }).then(console.log(req.body.name));
 });
 
-router.post("/api/proxy", (req, res) => {
-  axios
-    .get(
-      `https://api.yelp.com/v3/businesses/search?term=food&location=${req.body.location}`,
-      {
-        headers: {
-          Authorization: `Bearer ${key}`
-        },
-        responseType: `json`
-      }
-    )
-    .then(results => {
-      console.log(results.data);
-      res.render("index", {
-        data: results.data
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-
-router.get("/main", function (req, res) {
+router.get("/main", function(req, res) {
   res.render("index", req.user);
 });
 
-router.post("/api/match", function (req, res) {
-  console.log(req.body.name)
+router.post("/api/match", function(req, res) {
+  console.log(req.body.name);
 
   db.Resturant.create({
-    name: req.body.name,
-  }).catch( err =>{
-    res.send(req.body.name)
-  })
+    name: req.body.name
+  }).catch(err => {
+    res.send(req.body.name);
+  });
 });
 
 module.exports = router;
