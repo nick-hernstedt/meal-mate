4abbb1878971050769cd4718ea98dd80b88bf86e
Kayla Ward <kaylaw0127@gmail.com>
2020-04-13 17:11:52 -0700

    add toggle when no btn is clicked

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -2,79 +2,74 @@ const no = document.getElementById(`nono`);
 
 const yes = document.getElementById(`yes`);
 
-// no.onclick = () => {
-//     event.preventDefault()
-//     console.log(`buttsnstuff`)
-// }
-
-// no.addEventListener("click", () => {
-//     event.preventDefault()
-//     console.log(`buttsnstuff`)
-//     //find()
-// })
 let j = 0
 
-
 $("#search").on("click", function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  var location = {
-    location: $("#locationSearch")
-      .val()
-      .trim()
-  };
+    var location = {
+        location: $("#locationSearch")
+            .val()
+            .trim()
+    };
 
-  console.log(location);
+    console.log(location);
 
-  $.ajax("/api/proxy", {
-    type: "POST",
-    data: location
-  }).then(function (data) {
-    console.log(data);
+    $.ajax("/api/proxy", {
+        type: "POST",
+        data: location
+    }).then(function (data) {
+        console.log(data);
 
-    for (let i = 0; i < 20; i++) {
-      var choices = data.businesses[i];
+        for (let i = 0; i < 20; i++) {
+            var choices = data.businesses[i];
 
-      $(`#name${[i]}`).text(choices.name);
-      $(`#yes${[i]}`).attr("restaurant", choices.name);
-      $(`#imgsrc${[i]}`).attr("src", choices.image_url);
-      $(`#address${[i]}`).text(choices.location.display_address);
-      $(`#number${[i]}`).text(choices.display_phone);
-      $(`#reviews${[i]}`).text(choices.rating);
-    }
+            $(`#name${[i]}`).text(choices.name);
+            $(`#yes${[i]}`).attr("restaurant", choices.name);
+            $(`#imgsrc${[i]}`).attr("src", choices.image_url);
+            $(`#address${[i]}`).text(choices.location.display_address);
+            $(`#number${[i]}`).text(choices.display_phone);
+            $(`#reviews${[i]}`).text(choices.rating);
+        }
 
-    $(`#card${j}`).toggleClass("is-hidden")
-    j += 1
-  });
+        $(`#card${j}`).toggleClass("is-hidden")
+        j += 1
+    });
 });
 
 $(`.yes`).on("click", function () {
-  event.preventDefault();
+    event.preventDefault();
 
-  var match = {
-    name: $(this).attr("restaurant")
-  };
+    var match = {
+        name: $(this).attr("restaurant")
+    };
 
-  console.log($(this).attr("restaurant"));
+    console.log($(this).attr("restaurant"));
 
-  $.ajax("/api/match", {
-    type: "post",
-    data: match
-  }).then((data) => {
-    if (data != $(this).attr("restaurant")) {
-      console.log(`clciked`)
-      $(`#card${j-=1}`).toggleClass("is-hidden")
-      $(`#card${j += 1}`).toggleClass("is-hidden")
+    $.ajax("/api/match", {
+        type: "post",
+        data: match
+    }).then((data) => {
+        if (data != $(this).attr("restaurant")) {
+            console.log(`yes clicked`)
+            $(`#card${j-=1}`).toggleClass("is-hidden")
+            $(`#card${j += 1}`).toggleClass("is-hidden")
 
-      j += 1
+            j += 1
 
-    }
-    else if (data == $(this).attr("restaurant")) {
-      alert(`eat at ` + $(this).attr("restaurant"))
-    }
+        } else if (data == $(this).attr("restaurant")) {
+            alert(`eat at ` + $(this).attr("restaurant"))
+        }
+    });
+});
 
+$(`.nono`).on("click", function () {
+    event.preventDefault();
 
-  }
-  ); 
-}); 
+    console.log(`no clicked`)
+    $(`#card${j-=1}`).toggleClass("is-hidden")
+    $(`#card${j+= 1}`).toggleClass("is-hidden")
 
+    j += 1
+
+});
