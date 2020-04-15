8a7af6c47a3cc3900b26ac77992a96adcf97f72e
copiousamounts <nick.hernstedt@gmail.com>
2020-04-14 19:24:57 -0700

    setting up heroku and jaws db

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,3 @@ DROP DATABASE IF EXISTS meal_db;
 CREATE DATABASE meal_db;
 USE meal_db;
 
-CREATE TABLE match
-(
-	id int NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-);
